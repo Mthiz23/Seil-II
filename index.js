315350cd95c4f338419e34969eb278ca91bcc5bf
Matheus <162715010+Mthiz23@users.noreply.github.com>
2024-07-05 10:47:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function alertYes(){
-    alert("Obrigado, por me dá 10 centavos")
+function sim(){
+    alert("Obrigado, por me dá 10 centavos");
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
 }
